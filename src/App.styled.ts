a22ddea7efb8417eea21371d202418f9b8cbef4c
Marcelo Lima <marcelo.llima135@gmail.com>
2023-03-12 23:46:04 -0300

    add lists

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const App = styled.div`
   width: 100%;
   height: 100%;
-  background-color: #202020;
+  background-color: #111;
 `
 
+export const Lists = styled.section`
+  margin-top: -150px;
+`
