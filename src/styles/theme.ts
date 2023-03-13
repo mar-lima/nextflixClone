37903a4d855eae1e915619edc77c1802b4e34340
Marcelo Lima <marcelo.llima135@gmail.com>
2023-03-12 23:45:52 -0300

    them

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "light",
+  colors: {
+    primary: "#E54F6D",
+    secundaty: "#451F55",
+    violet: "#724e91",
+
+    background: "#22162B",
+    text: "#f8c630",
+  },
+};
